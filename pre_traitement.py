d889cde0625480f18d9466b3db95453d0b4a53f7
Virgile <60672639+SuperVirgile@users.noreply.github.com>
2022-12-30 12:46:12 +0100

    Update pre_traitement.py

--- pre_traitement.py
+++ pre_traitement.py
@@ -5,15 +5,16 @@ client = MongoClient('localhost', 27017)
 db = client['Cartier']
 collection = db['Virgile']
 
+# Création des coordonnées
 def creation_coordonnees(collection):
-    #Cela nous permet d'itérer sur chaque document
+  
     documents = collection.find()
     for doc in documents:
-        #On récupère les longitudes et latitudes dans chacun de nos documents
+     
         longitude = float(doc['longitude'])
         latitude = float(doc['latitude'])
-        #Nous faisons ici notre update en faisant passer les valeurs récupérés plus haut - on s'assure de bien updater l'ID de chaque document
-        #grâce à la variable doc['_id'] !
+        
+
         collection.update({"_id":doc['_id']},{"$set": {"loc" :
             {"type":"Point",
             "coordinates" : [
